1a80be295269341ca80869067ddd7e7a174d3010
Gurunadh552 <gurunadhpukkalla@gmail.com>
2023-02-12 23:58:04 +0530

    Added ENV

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      host: env("DATABASE_HOST", "sql673.main-hosting.eu"),
-      port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "u730133489_simplified_nin"),
-      user: env("DATABASE_USERNAME", "u730133489_ninja"),
-      password: env("DATABASE_PASSWORD", "~y7gFgtgV1K"),
+      host: env("DATABASE_HOST", process.env.DATABASE_HOST),
+      port: env.int("DATABASE_PORT", process.env.DATABASE_PORT),
+      database: env("DATABASE_NAME", process.env.DATABASE_NAME),
+      user: env("DATABASE_USERNAME", process.env.DATABASE_USERNAME),
+      password: env("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD),
       ssl: env.bool("DATABASE_SSL", true),
     },
   },
--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,43 @@
+import { config } from "dotenv";
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-      jwtSecret: env('JWT_SECRET'),
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
+  },
+
+  // for email
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", process.env.SMTP_HOST),
+        port: env("SMTP_PORT", process.env.SMTP_PORT),
+        auth: {
+          user: process.env.SMTP_USERNAME,
+          pass: process.env.SMTP_PASSWORD,
         },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+        secure: true,
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: process.env.DEFAULT_EMAIL,
+        defaultReplyTo: process.env.DEFAULT_EMAIL,
       },
     },
-  });
+  },
+});
